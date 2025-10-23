ceeed54326dd49937b01ea4670f7c1abe5bcbab6
ConnieEddie <cedwards@icstars.org>
2025-10-23 18:40:20 -0500

    Commit 3
    "refactored with clear variable and comments"

--- understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
+++ understanding-js-fundamentals/JS-Katas-Everyone/Kata2.js
@@ -4,6 +4,26 @@
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
-for (let i = 1; i <= 20; i++) {
-  
+ for (let i = 1; i <= 30; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+for (let a = 1; a >= 20; a-- ) {
+if (a % 5 === 0 && a % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (a % 5 === 0) {
+    console.log("Fizz");
+  } else if (a % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(a);
+  }
 }
